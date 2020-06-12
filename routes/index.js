80c27f8428ff5423635a9a2a82aa647011142274
Micha van Eijk <micha@getroadmap.com>
2020-06-12 13:54:49 +0200

    Do something with variables, to see if it's working

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 
+router.get('/:username', (req, res) => {
+	res.render('index', req.params);
+});
+
 router.get('/', (req, res) => {
   res.render('index');
-
 });
 
 router.get('/yolo', (req, res) => {
