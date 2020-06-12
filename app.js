0a155c5b098bf22fbb2ca7685b9dbc9390b6d3ee
Niels Koole <niels@getroadmap.com>
2020-06-12 12:35:13 +0200

    Should give a response

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
+const cool = require('cool-ascii-faces');
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .get('/', (req, res) => 'yooo broo')
+  .get('/', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
