101592c5311b33cb2b8d47cde272a152bd823f37
Niels Koole <niels@getroadmap.com>
2020-06-14 21:04:48 +0200

    Trusted suppliers extended

--- routes/index.js
+++ routes/index.js
@@ -4,23 +4,13 @@ const router = express.Router();
 
 router.get('/advice', async (req, res, next) => {
 	try {
-		const response = await got('https://travelscrum.herokuapp.com/api/', req);
+		const response = await got('https://travelscrum.herokuapp.com/api/', req.query);
 		const obj = JSON.parse(response.body);
 		res.render('advice', obj);
 	} catch (err) {
 		next(err);
 	}
 });
-
-// router.get('/advice', async (req, res, next) => {
-// 	try {
-// 		const response = await got('https://travelscrum.herokuapp.com/api/?cityName=' + req.destination);
-// 		const obj = JSON.parse(response.body);
-// 		res.render('advice', obj);
-// 	} catch (err) {
-// 		next(err);
-// 	}
-// });
 
 router.get('/entry', (req, res) => {
 	res.render('entry', req.params);
