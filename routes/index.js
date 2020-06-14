c28796315e8d162694419576675670ca8b5c225e
Niels Koole <niels@getroadmap.com>
2020-06-14 20:34:39 +0200

    Working on trusted partners

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const got = require('got');
+const got = require('g');
 const router = express.Router();
 
 router.get('/advice', async (req, res, next) => {
 	try {
-		const response = await got('https://travelscrum.herokuapp.com/api/?cityName=' + req.destination);
+		const response = await got('https://travelscrum.herokuapp.com/api/', req);
 		const obj = JSON.parse(response.body);
 		res.render('advice', obj);
 	} catch (err) {
@@ -12,9 +12,15 @@ router.get('/advice', async (req, res, next) => {
 	}
 });
 
-router.get('/test', (req, res) => {
-	res.render('advice');
-});
+// router.get('/advice', async (req, res, next) => {
+// 	try {
+// 		const response = await got('https://travelscrum.herokuapp.com/api/?cityName=' + req.destination);
+// 		const obj = JSON.parse(response.body);
+// 		res.render('advice', obj);
+// 	} catch (err) {
+// 		next(err);
+// 	}
+// });
 
 router.get('/entry', (req, res) => {
 	res.render('entry', req.params);
