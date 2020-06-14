ba5d1db2b80f7a38f7b40ede0f2a3388afad6483
Micha van Eijk <micha@getroadmap.com>
2020-06-14 19:58:14 +0200

    Fix something with new modules!

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -4,5 +4,12 @@ module.exports = {
 		if (typeof aString !== 'string') return ''
 	
 		return aString.charAt(0).toUpperCase() + aString.slice(1)
+	},
+	moment: function () {
+		
+		var date = new Date();
+		
+		return date.getTime();
+		
 	}
 }
