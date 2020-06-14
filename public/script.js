a9e3cd11ac5b2b399177056834d51084344f9a79
Micha van Eijk <micha@getroadmap.com>
2020-06-15 01:03:37 +0200

    Update script.js

--- public/script.js
+++ public/script.js
@@ -35,3 +35,13 @@ var changeEmo = function (type) {
 	
 	$('#checkboxField').css ('display', 'block');
 };
+
+var a=document.getElementsByTagName("a");
+for(var i=0;i<a.length;i++)
+{
+    a[i].onclick=function()
+    {
+        window.location=this.getAttribute("href");
+        return false
+    }
+}
